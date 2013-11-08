869a09cf8613aa38f8ba09490124ccd0f29c2c53
James Mosier <jamesdmosier@gmail.com>
2013-11-07 19:42:13 -0500

    removed controller logic from app.js

--- app/js/app.js
+++ app/js/app.js
@@ -4,46 +4,19 @@
 // Declare app level module which depends on filters, and services
 angular.module('cps', [
   'ngRoute',
+  'cpsController'
   //'cps.filters',
   //'cps.services',
   //'cps.directives',
-  //'controllers'
+  //'cps.controllers'
 ]).
 config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/home', { templateUrl: '/app/home.html', controller: 'HomeCtrl', activeTab: 'home' });
     $routeProvider.when('/gallery', { templateUrl: '/app/gallery.html', controller: 'HomeCtrl', activeTab: 'gallery' });
-    $routeProvider.when('/services:slug', { templateUrl: '/app/services.html', controller: 'ServicesCtrl', activeTab: 'services' });
-    $routeProvider.when('/contact:slug', { templateUrl: '/app/contact.html', controller: 'ContactCtrl', activeTab: 'contact' });
+    $routeProvider.when('/services', { templateUrl: '/app/services.html', controller: 'ServicesCtrl', activeTab: 'services' });
+    $routeProvider.when('/contact', { templateUrl: '/app/contact.html', controller: 'ContactCtrl', activeTab: 'contact' });
     $routeProvider.otherwise({ redirectTo: '/home' });
 }]);
 
-//var CPSSite = angular.module('CPSSite', []);
-//
-//CPSSite.config(function ($routeProvider) {
-//  $routeProvider
-//    .when('/page/:slug', {templateUrl: 'partials/page.html', controller: 'RouteController'})
-//    .otherwise({redirectTo: '/page/home'});
-//});
-//
-function HomeCtrl($scope, $rootScope, $http, $route) {
-    // Load pages on startup
-    $http.get('/app/home.html').success(function (data) {
-        $rootScope.pages = data;
-        
-    });
-    $scope.$route = $route;
-    
-    // Set the slug for menu active class
-    //  $scope.$on('routeLoaded', function (event, args) {
-    //      alert('foo');
-    //    $scope.slug = args.slug;
-    //  });
-}
-//
-//function RouteController ($scope, $rootScope, $routeParams) {
-//  // Getting the slug from $routeParams
-//  var slug = $routeParams.slug;
-//  
-//  $scope.$emit('routeLoaded', {slug: slug});
-//  $scope.page = $rootScope.pages[slug];
-//}
+
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,23 +2,24 @@
 
 /* Controllers */
 
-angular.module('cps.controllers', []).
-  controller('HomeCtrl', [function() {
-  $http.get('/home.html').success(function (data) {
-    $rootScope.pages = data;
-  });
-
-  // Set the slug for menu active class
-//  $scope.$on('routeLoaded', function (event, args) {
-//    $scope.slug = args.slug;
-//  });
-  }])
-  .controller('GalleryCtrl', [function() {
-
+var cpsController = angular.module('cpsController', []);
+ 
+cpsController.controller('HomeCtrl', ['$scope', '$rootScope', '$http', '$route',
+  function HomeCtrl($scope, $rootScope, $http, $route) {
+    // Load pages on startup
+    $http.get('/app/home.html').success(function (data) {
+        $rootScope.pages = data;
+        
+    });    
+    //passes $route to the view for activeTab
+    $scope.$route = $route;
   }]);
-  .controller('ServicesCtrl', [function() {
-
-  }]);
-  .controller('ContactCtrl', [function() {
-
+ 
+cpsController.controller('GalleryCtrl', ['$scope', '$routeParams',
+  function($scope, $http) {
+    $http.get('data/gallery.json').success(function(data) {
+      $scope.gallery = data;
+    });
+ 
+    
   }]);
