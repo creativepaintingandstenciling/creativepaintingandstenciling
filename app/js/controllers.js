7dd9e3a62783e347326fc93c8e5ba5eed8e7cc78
James Mosier <jamesdmosier@gmail.com>
2013-11-10 18:59:01 -0500

    added form validation to contact form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -23,3 +23,33 @@ cpsController.controller('GalleryCtrl', ['$scope', '$http', '$routeParams',
  
     
   }]);
+
+cpsController.controller('ServicesCtrl', ['$scope', '$rootScope', '$http', '$route',
+  function ServicesCtrl($scope, $rootScope, $http, $route) {
+    // Load pages on startup
+    $http.get('/app/services.html').success(function (data) {
+        $rootScope.pages = data;
+        
+    });    
+    //passes $route to the view for activeTab
+    $scope.$route = $route;
+  }]);
+
+cpsController.controller('ContactCtrl', ['$scope', '$rootScope', '$http', '$route',
+  function ContactCtrl($scope, $rootScope, $http, $route) {
+    // Load pages on startup
+    $http.get('/app/contact.html').success(function (data) {
+        $rootScope.pages = data;
+        
+    });    
+    //passes $route to the view for activeTab
+    $scope.$route = $route;
+
+    $scope.username1 = 'Peter Parker';
+    $scope.email1 = 'pparker@gmail.com';
+
+    $scope.submitForm = function () {
+        console.info("Here I should implement the logic to send a request to the server.");
+    }
+
+  }]);
