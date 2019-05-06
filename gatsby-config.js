b34206d57be6e32e2acd9e6d2eafac7a9ea1e6e1
James Mosier <james@jamesdmosier.com>
2019-05-05 22:14:48 -0400

    removing starter css/js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Creative Painting & Stenciling`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,8 +29,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Lora\:400,700`, `Satisfy`],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
+/* eslint-enable */
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,25 @@
+import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import styled from 'styled-components';
+
+import { Container } from '../styles/core';
+
+const Heading = styled.h1`
+  font-family: 'Satisfy';
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Container>
+      <Heading>
+        <Link to="/">{siteTitle}</Link>
+      </Heading>
+    </Container>
   </header>
 );
 
